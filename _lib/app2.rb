94fd7f35d5b7e166dfd85846f11fb2203bd50419
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-03-10 23:30:56 +0900

    Update app2.rb

--- _lib/app2.rb
+++ _lib/app2.rb
@@ -5,7 +5,7 @@ require_relative 'itunessongs'
 puts "Apple Music - Top songs<br />"
 
 ranking_data = get_ranking_data(10)
-extract_ranking(ranking_data).each_with_index(1) do |song, rank|
+extract_ranking(ranking_data).each.with_index(1) do |song, rank|
   s = Song.new(
     song['name'],
     song['artistName'],
