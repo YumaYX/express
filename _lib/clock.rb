7850cf7cb11b4ca601a424f43593cd40d0e1151e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-03-15 00:47:25 +0900

    clock.rb の更新

--- _lib/clock.rb
+++ _lib/clock.rb
@@ -1,2 +1,25 @@
-ENV['TZ'] = 'Asia/Tokyo'
-puts Time.now
+require 'time'
+
+def time_to_emoji(time)
+  hour = time.hour
+  minute = time.min
+
+  if minute.between?(0, 15) || minute.between?(45, 59)
+    # オクロック（ちょうどの時間）を表示
+    emoji_index = hour % 12
+  else
+    # 30分（ハーフ）の時間を表示
+    emoji_index = hour % 12 + 12
+  end
+
+  emojis = ["🕛", "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚",
+            "🕧", "🕜", "🕝", "🕞", "🕟", "🕠", "🕡", "🕢", "🕣", "🕤", "🕥", "🕦"]
+
+  emojis[emoji_index]
+end
+
+# 現在の日本時間（Asia/Tokyo）を取得
+tokyo_time = Time.now.getlocal("+09:00")
+
+# 絵文字を出力
+puts "# #{time_to_emoji(tokyo_time)}"
