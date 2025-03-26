61920cfdf9175261c36e595ad4bc36c1328c7997
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-03-26 22:47:40 +0900

    clock.rb の更新

--- _lib/clock.rb
+++ _lib/clock.rb
@@ -23,5 +23,35 @@ end
 # 現在の日本時間（Asia/Tokyo）を取得
 tokyo_time = Time.now.getlocal("+09:00")
 
+def sky_brightness(hh)
+  case hh
+  when 4 then puts "🌄"  
+  when 5 then puts "🌅"  
+  when 6 then puts "🌞"  
+  when 7 then puts "🏙️"  
+  when 8 then puts "🌤"  
+  when 9 then puts "🌞"  
+  when 10 then puts "☀️"  
+  when 11 then puts "🔆"  
+  when 12 then puts "🍽️"
+  when 13 then puts "🏖️"  
+  when 14 then puts "🌤️"  
+  when 15 then puts "🌥️"  
+  when 16 then puts "🌇"  
+  when 17 then puts "🏞️"  
+  when 18 then puts "🌆"  
+  when 19 then puts "🌓"  
+  when 20 then puts "🌃"  
+  when 21 then puts "🌠"  
+  when 22 then puts "✨"  
+  when 23 then puts "🌙"  
+  when 0 then puts "💤"  
+  when 1 then puts "🦉"  
+  when 2 then puts "🌌"  
+  when 3 then puts "🌑"  
+  else puts "❓"  
+  end
+end
+
 # 絵文字を出力
-puts "# #{time_to_emoji(tokyo_time)}"
+puts "# #{time_to_emoji(tokyo_time)}#{sky_brightness(tokyo_time.hour)}"
