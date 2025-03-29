0381ac83ea86b4a754053ef43add9bf1b9e00701
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-03-29 23:45:51 +0900

    app2.rb を更新

--- _lib/app2.rb
+++ _lib/app2.rb
@@ -2,7 +2,7 @@
 
 require_relative 'itunessongs'
 
-puts "Apple Music - Top songs<br />"
+puts "🍎Apple Music - Top Songs<br />"
 
 ranking_data = get_ranking_data(10)
 extract_ranking(ranking_data).each.with_index(1) do |song, rank|
