53f90d097c2f80fef33c2756b8ac971f2a23a04a
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-04-03 21:37:57 +0900

    Create year.rb

---
+++ _lib/year.rb
@@ -0,0 +1,14 @@
+require 'time'
+
+def year_progress 
+  now = Time.now.getlocal("+09:00") # 日本時間（JST） 
+  start_of_year = Time.new(now.year, 1, 1, 0, 0, 0, "+09:00") 
+  end_of_year = Time.new(now.year + 1, 1, 1, 0, 0, 0, "+09:00")
+  elapsed_seconds = now - start_of_year 
+  total_seconds = end_of_year - start_of_year
+  progress = (elapsed_seconds / total_seconds * 100).round(2)
+
+  puts "今年の進捗率: #{progress}%"
+end
+
+year_progress
